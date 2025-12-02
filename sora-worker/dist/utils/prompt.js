6a7e0007ec5dab61a5af3d65e63f28531960f2fd
kenroly <ngthedat2002@gmail.com>
2025-12-02 15:00:37 +0700

    Update

--- sora-worker/dist/utils/prompt.js
+++
@@ -1,8 +0,0 @@
-import readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
-export async function ask(question) {
-    const rl = readline.createInterface({ input, output });
-    const answer = await rl.question(question);
-    rl.close();
-    return answer;
-}
